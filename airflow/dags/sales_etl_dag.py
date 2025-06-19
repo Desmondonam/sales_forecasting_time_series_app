46c91dd376ccae166eeb35403c3c2c4a0a128b57
Desmondonam <desmondonam@gmail.com>
2025-06-19 14:27:57 -0400

    add docker files compose and update requirements file

--- airflow/dags/sales_etl_dag.py
+++ airflow/dags/sales_etl_dag.py
@@ -9,7 +9,7 @@ def generate_sales_data():
     dates = pd.date_range(today - pd.Timedelta(days=60), today)
     sales = [round(1000 + random.gauss(0, 50), 2) for _ in range(len(dates))]
     df = pd.DataFrame({"date": dates, "sales": sales})
-    df.to_csv("synthetic_sales.csv", index=False)
+    df.to_csv("/opt/airflow/dags/synthetic_sales.csv", index=False)
 
 with DAG("pharma_sales_etl", start_date=datetime(2023, 1, 1), schedule_interval="@daily", catchup=False) as dag:
     etl_task = PythonOperator(
